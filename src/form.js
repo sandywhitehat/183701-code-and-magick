8c134777cc7aedca96ee321d525169d31406a724
sandywhitehat <sandypandaadv@outlook.com>
2016-06-13 14:27:00 +0300

    Использование oninput для валидации(?)
    Воспользовался oninput (да, я оооочень долго соображал), чтобы заставить
    жертву моего кода не оставлять поле ввода имени и отзыва пустым. Правда
    тут же вопрос - оформлено у меня это в виде алертов и форму отправить
    можно, не заполняя полей в итоге . То есть конкретно соответствия
    атрибуту required в html мне не добиться (во всяком случаи мой ресёрч не
    дал мне ответа, пока что).

--- src/form.js
+++ src/form.js
@@ -15,3 +15,23 @@
     formContainer.classList.add('invisible');
   };
 })();
+
+var reviewNameField = document.getElementById('review-name');
+reviewNameField.oninput = function() {
+  if (reviewNameField.value == '') {
+    alert('Введите имя, пожалуйста!');
+    return false;
+  }
+}
+
+var reviewTextField = document.getElementById('review-text');
+var reviewMarkField = document.getElementsByName('review-mark');
+reviewTextField.oninput = function() {
+  if (reviewMarkField.value = 1, 2) {
+    if (reviewTextField.value == '') {
+    alert('Попрошу объясниться, товарищ!');
+    return false;
+  };
+  return false;
+};
+};
