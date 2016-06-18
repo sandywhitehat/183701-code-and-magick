ded26592782fdada81738e6e7ba1f128d06aec5a
sandywhitehat <sandypandaadv@outlook.com>
2016-06-18 15:47:06 +0300

    Задание №9
    Слегка изменил сценарии, теперь кнопка сабмита доступна и при оценке
    выше 2

--- src/form.js
+++ src/form.js
@@ -17,21 +17,64 @@
 })();
 
 var reviewNameField = document.getElementById('review-name');
+var reviewTextField = document.getElementById('review-text');
+var rate = document.getElementsByName('review-mark');
+
+var lable1 = document.getElementsByClassName('review-fields');
+var link1 = document.getElementsByClassName('review-fields-name');
+var link2 = document.getElementsByClassName('review-fields-text');
+var submit = document.getElementsByClassName('review-submit');
+submit[0].setAttribute('disabled', '');
+function checkValidity() {
+  submit[0].removeAttribute('disabled', '');
+}
 reviewNameField.oninput = function() {
-  if (reviewNameField.value == '') {
-    alert('Введите имя, пожалуйста!');
+
+  if (reviewNameField.value === '') {
+    reviewNameField.setAttribute('required', '');
+    link1[0].classList.remove('invisible');
+    lable1[0].classList.remove('invisible');
+    return false;
+  } else if (!(reviewNameField.value === '') && !(reviewTextField.value === '')) {
+    checkValidity();
+    lable1[0].classList.add('invisible');
+    return true;
+  } else if ((rate[2].checked || rate[3].checked || rate[4].checked) && !(reviewNameField.value === '')) {
+    checkValidity();
+    reviewTextField.removeAttribute('required', '');
+    link2[0].classList.remove('invisible');
+    lable1[0].classList.remove('invisible');
+    return true;
+  } else {
+    link1[0].classList.add('invisible');
+    return true;
+  }
+};
+
+reviewTextField.oninput = function() {
+  if ((rate[1].checked || rate[0].checked) && reviewTextField.value === '') {
+    reviewTextField.setAttribute('required', '');
+    link2[0].classList.remove('invisible');
+    lable1[0].classList.remove('invisible');
+    return false;
+  } else if (!(reviewNameField.value === '') && !(reviewTextField.value === '')) {
+    checkValidity();
+    lable1[0].classList.add('invisible');
+    return true;
+  } else if ((rate[2].checked || rate[3].checked || rate[4].checked) && (reviewTextField.value === '')) {
+    link2[0].classList.add('invisible');
+  } else {
+    link2[0].classList.add('invisible');
+    return true;
+  }
+  return true;
+};
+
+submit.onclick = function(event) {
+  event.preventDefault();
+  if (reviewNameField.value === '') {
+    console.log('Введите имя, пожалуйста.');
     return false;
   }
-}
-
-var reviewTextField = document.getElementById('review-text');
-var reviewMarkField = document.getElementsByName('review-mark');
-reviewTextField.oninput = function() {
-  if (reviewMarkField.value = 1, 2) {
-    if (reviewTextField.value == '') {
-    alert('Попрошу объясниться, товарищ!');
-    return false;
-  };
-  return false;
-};
+  return true;
 };
