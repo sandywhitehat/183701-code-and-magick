829aa20bd32bfc2b82c04515c5650fe4ab870ef0
sandywhitehat <sandypandaadv@outlook.com>
2016-06-18 19:24:11 +0300

    Задание №9
    Немного доработал сценарии инейбла сабмита

--- src/form.js
+++ src/form.js
@@ -15,3 +15,66 @@
     formContainer.classList.add('invisible');
   };
 })();
+
+var reviewNameField = document.getElementById('review-name');
+var reviewTextField = document.getElementById('review-text');
+var rate = document.getElementsByName('review-mark');
+
+var lable1 = document.getElementsByClassName('review-fields');
+var link1 = document.getElementsByClassName('review-fields-name');
+var link2 = document.getElementsByClassName('review-fields-text');
+var submit = document.getElementsByClassName('review-submit');
+submit[0].setAttribute('disabled', '');
+function checkValidity() {
+  submit[0].removeAttribute('disabled', '');
+}
+reviewNameField.oninput = function() {
+
+  if (reviewNameField.value === '') {
+    reviewNameField.setAttribute('required', '');
+    link1[0].classList.remove('invisible');
+    lable1[0].classList.remove('invisible');
+    return false;
+  } else if (!(reviewNameField.value === '') && !(reviewTextField.value === '')) {
+    checkValidity();
+    lable1[0].classList.add('invisible');
+    return true;
+  } else if ((rate[2].checked || rate[3].checked || rate[4].checked) && !(reviewNameField.value === '')) {
+    checkValidity();
+    reviewTextField.removeAttribute('required', '');
+    link2[0].classList.remove('invisible');
+    lable1[0].classList.remove('invisible');
+    return true;
+  } else {
+    link1[0].classList.add('invisible');
+    return true;
+  }
+};
+
+reviewTextField.oninput = function() {
+  if ((rate[1].checked || rate[0].checked) && reviewTextField.value === '') {
+    reviewTextField.setAttribute('required', '');
+    link2[0].classList.remove('invisible');
+    lable1[0].classList.remove('invisible');
+    return false;
+  } else if (!(reviewNameField.value === '') && !(reviewTextField.value === '')) {
+    checkValidity();
+    lable1[0].classList.add('invisible');
+    return true;
+  } else if ((rate[2].checked || rate[3].checked || rate[4].checked) && (reviewTextField.value === '')) {
+    link2[0].classList.add('invisible');
+  } else {
+    link2[0].classList.add('invisible');
+    return true;
+  }
+  return true;
+};
+
+submit.onclick = function(event) {
+  event.preventDefault();
+  if (reviewNameField.value === '') {
+    console.log('Введите имя, пожалуйста.');
+    return false;
+  }
+  return true;
+};
