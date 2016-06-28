24ed34b04bdbe6612954a9445c48a8e62b94ed2a
sandywhitehat <sandypandaadv@outlook.com>
2016-06-28 22:03:27 +0300

    ITS A FINAL COUNTDOWN
    Аватарски грузятся, рейтинг отображается

--- src/reviews.js
+++ src/reviews.js
@@ -13,19 +13,36 @@ if ('content' in templateElement) {
   elementToClone = templateElement.querySelector('.review');
 }
 
+var setRating = function(number, container) {
+
+  var markContainer = ['two', 'three', 'four', 'five'];
+  // 2 соответствует элементу массива 0
+  // 3 соотвествует элементу массива 1
+  // 4 соотвествует элементу массива 2
+  // 5 соотвествует элементу массива 3
+  var reviewMark = markContainer[number - 2];
+
+  container.classList.add('review-rating-' + reviewMark);
+
+};
+
 var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
-  element.querySelector('.review-rating').textContent = data.rating;
+  setRating(data.rating, element.querySelector('.review-rating'));
+//   element.querySelector('.review-rating').classList.add('review-rating-')
+
   element.querySelector('.review-text').textContent = data.description;
   container.appendChild(element);
 
-  var avatar = new Image();
-  avatar.onload = function() {
-    element.querySelector('img').insertAdjacentHTML('beforeend', '<img src="data.author.picture" width="124" height="124">');
+  var avatar = new Image(124, 124);
+  avatar.onload = function(evt) {
+    element.querySelector('.review-author');
+    element.querySelector('.review-author').src = evt.target.src;
   };
   avatar.onerror = function() {
     element.classList.add('review-load-failure');
   };
+  avatar.src = data.author.picture;
   return element;
 };
 window.reviews.forEach(function(reviewer) {
