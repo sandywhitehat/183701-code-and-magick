5dd210f5eb78a3ea5d4ef2674ffb57268daea717
sandywhitehat <sandypandaadv@outlook.com>
2016-06-30 23:12:07 +0300

    Npm test
    'reviews' is already declared in the upper scope  no-shadow
    С датой разобрался, но ESLINT выдает ошибку, вызывающую у меня конфуз

--- src/reviews.js
+++ src/reviews.js
@@ -3,8 +3,10 @@
 var reviewsFilter = document.querySelector('.reviews-filter');
 reviewsFilter.classList.add('invisible');
 
+var filtersContainer = document.querySelector('.reviews-filter');
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
+var reviewBlock = document.querySelector('.reviews');
 var elementToClone;
 
 if ('content' in templateElement) {
@@ -12,6 +14,24 @@ if ('content' in templateElement) {
 } else {
   elementToClone = templateElement.querySelector('.review');
 }
+
+/** @constant {string} */
+var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
+
+/** @type {Array.<Object>} */
+var reviews = [];
+
+var Filter = {
+  'ALL': 'reviews-all',
+  'DATE': 'reviews-recent',
+  'RATING_GOOD': 'reviews-good',
+  'RATING_BAD': 'reviews-bad',
+  'REVIEW_USEFULNESS': 'reviews-popular'
+};
+
+/** @constant {Filter} */
+var DEFAULT_FILTER = Filter.ALL;
+
 
 var setRating = function(number, container) {
 
@@ -45,7 +65,142 @@ var getReviewElement = function(data, container) {
   avatar.src = data.author.picture;
   return element;
 };
-window.reviews.forEach(function(reviewer) {
-  getReviewElement(reviewer, reviewsContainer);
-});
+
+/** @param {Array.<Object>} reviews */
+var renderReviews = function(reviews) {
+  reviewsContainer.innerHTML = '';
+
+  reviews.forEach(function(review) {
+    getReviewElement(review, reviewsContainer);
+  });
+};
+
+/**
+ * @param {Array.<Object>} reviews
+ * @param {Filter} filter
+ */
+var getFilteredReviews = function(reviews, filter) {
+  var reviewsToFilter = reviews.slice(0);
+
+  switch (filter) {
+
+    case Filter.DATE:
+
+      reviewsToFilter = reviewsToFilter.filter(function(review) {
+
+        var now = new Date();
+        var fourDaysAgo = new Date();
+        fourDaysAgo.setDate(now.getDate() - 4);
+        var reviewDate = Date.parse(review.date);
+        //console.log(reviewDate);
+        return reviewDate > fourDaysAgo;
+      }).sort(function(a, b) {
+        return b.reviewDate - a.reviewDate;
+      });
+      break;
+
+    case Filter.RATING_GOOD:
+      reviewsToFilter = reviewsToFilter.filter(function(review) {
+        return review.rating > 2;
+      }).sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+
+    case Filter.RATING_BAD:
+      reviewsToFilter = reviewsToFilter.filter(function(review) {
+        return review.rating < 3;
+      }).sort(function(a, b) {
+        return a.rating - b.rating;
+      });
+      break;
+
+    case Filter.REVIEW_USEFULNESS:
+      reviewsToFilter = reviewsToFilter.sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+      break;
+  }
+
+  return reviewsToFilter;
+};
+
+/** @param {Filter} filter */
+var setFilterEnabled = function(filter) {
+  console.log('приходящий фильтр ' + filter);
+  var filteredReviews = getFilteredReviews(reviews, filter);
+  renderReviews(filteredReviews);
+};
+  // пробегается по всем инпутам, находит выбранный и включает такой фильтр
+var checkOnChecked = function() {
+  var inputs = document.getElementsByName('reviews');
+  for (var i = 0; i < inputs.length; i++) {
+    var currentInput = inputs[i];
+    //console.log(currentInput);
+    if (currentInput.checked) {
+      setFilterEnabled(currentInput.id);
+    }
+  }
+};
+// мы находим элемент с нужным id и выделяем его
+var checkClickedInput = function(event) {
+  console.log('clicked');
+  // присваиваем переменной того, по кому кликаем
+  var lableClicked = event.target;
+  var inputId = lableClicked.getAttribute('for');
+  cleanCheckedChecks();
+  document.getElementById(inputId).setAttribute('checked', '');
+  checkOnChecked();
+
+};
+var cleanCheckedChecks = function() {
+  //находим все инпуты с именем reviews
+  var inputs = document.getElementsByName('reviews');
+  // пробегаемся по всем инпутам
+  for ( var i = 0; i < inputs.length; i++) {
+    // на каждый инпут создаём переменную
+    var currentInput = inputs[i];
+    // каждому инпуту убираем атрибут checked
+    currentInput.removeAttribute('checked', '');
+  }
+};
+/** @param {boolean} enabled */
+var setFiltersListeners = function() {
+  var filters = filtersContainer.querySelectorAll('.reviews-filter-item');
+  //console.log(filters);
+  for (var i = 0; i < filters.length; i++) {
+    var filter = filters[i];
+    filter.onclick = checkClickedInput;
+
+
+  }
+};
+
+/** @param {function(Array.<Object>)} callback */
+var getReviews = function(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onprogress = function() {
+    reviewBlock.classList.add('reviews-list-loading');
+  };
+  // getReview -> onload - callback
+  xhr.onload = function(evt) {
+    reviewBlock.classList.remove('reviews-list-loading');
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+  };
+  xhr.onerror = function() {
+    reviewBlock.classList.remove('reviews-list-loading');
+    reviewBlock.classList.add('reviews-load-failure');
+  };
+
+  xhr.open('GET', REVIEWS_LOAD_URL);
+  xhr.send();
+};
+var getReviewsCallback = function(loadedData) {
+  reviews = loadedData;
+  setFiltersListeners(true);
+  setFilterEnabled(DEFAULT_FILTER);
+};
+getReviews(getReviewsCallback);
+
 reviewsFilter.classList.remove('invisible');
